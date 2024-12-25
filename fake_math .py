7b7a5b024d22d184bcffb3adcaff6884a38f9d34
yahab27 <yatol27@yahoo.com>
2024-12-25 14:03:46 +1000

    Add files via upload

---
+++ fake_math .py
@@ -0,0 +1,10 @@
+def divide(first, second):
+   # if second == 0:
+      #  return 'Ошибка'
+    #else:
+     #   return first / second
+
+#
+#if __name__ == '__main__':
+  #  result = divide(69, 0)
+    print('result')
---
+++ moodle_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as m # вызов модулей
+from true_math import divide as n
+
+result1 = m(69, 3)
+result2 = m(3, 0)
+result3 = n(49, 7)
+result4 = n(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
